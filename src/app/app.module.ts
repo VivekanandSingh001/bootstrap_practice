0178eab281b84f5335ff5ee6d0d427ab7deda8dd
VivekanandSingh001 <vivekanandsingh@nimapinfotech.com>
2023-07-11 18:32:33 +0530

    practise2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { HomeComponent } from './home/home.component';
     ContentComponent,
     FooterComponent,
     LoginComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
