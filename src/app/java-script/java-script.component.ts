00a5b106038255c7072d3aecb0e92adb28f1bb38
VivekanandSingh001 <vivekanandsingh@nimapinfotech.com>
2023-07-14 18:41:17 +0530

    angular_practice5

--- src/app/java-script/java-script.component.ts
+++ src/app/java-script/java-script.component.ts
@@ -9,6 +9,9 @@ export class JavaScriptComponent {
   abc = ["t", "h", "i", "s"];
   a: any[] = [];
  sum=0;
+ str="";
+ str1="";
+ str2="";
   yo() {
     for (let i = 0; i < this.abc.length; i++) {
       if (i % 2 === 0) {
@@ -23,4 +26,98 @@ this.sum+=j;
     }
     console.log("sum",this.sum);
   }
+  
+// let n = 5;
+// let string = "";
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += i+j;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+convertToBinary(){
+  let n = 5;
+let string = "";
+for (let i = 0; i < n; i++) {
+  
+  for (let j = 0; j < i; j++) {
+   this.str= string += " ";
+  }
+  for (let k = 0; k < 2 * (n-i) - 1; k++) {
+    this.str= string += "*";
+  }
+  this.str= string += "\n";
+}
+console.log(this.str);
+}
+
+
+convertToBinary1(){
+  let n = 5;
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    
+    for (let j = n; j > i; j--) {
+     this.str1= string += " ";
+    }
+    for (let k = 0; k < i * 2 - 1; k++) {
+     this.str1= string += "*";
+    }
+   this.str1= string += "\n";
+  }
+  for (let i = 1; i <= n - 1; i++) {
+    
+    for (let j = 0; j < i; j++) {
+     this.str1= string += " ";
+    }
+    for (let k = (n - i) * 2 - 1; k > 0; k--) {
+      this.str1=string += "*";
+    }
+   this.str1= string += "\n";
+  }
+  console.log(string);
+}
+convertToBinary2(){
+  let n = 5;
+let string = "";
+for (let i = 0; i < n; i++) {
+  
+  for (let j = 0; j < i; j++) {
+   this.str2= string += " ";
+  }
+  for (let k = 0; k < (n - i) * 2 - 1; k++) {
+    this.str2=  string += "*";
+  }
+  this.str2=string += "\n";
+}
+for (let i = 2; i <= n; i++) {
+  
+  for (let j = n; j > i; j--) {
+    this.str2= string += " ";
+  }
+  for (let k = 0; k < i * 2 - 1; k++) {
+    this.str2=  string += "*";
+  }
+  this.str2=  string += "\n";
+}
+console.log(this.str2);
+}
+// proxy(){
+//   let student1 = {
+//     age: 24,
+//     name: "Felix"
+// }
+
+// const handler = {
+//     get: function(obj, prop) {
+//         return obj[prop] ? obj[prop] : 'property does not exist';
+//     }
+// }
+
+// const proxy = new Proxy(student1, handler);
+// console.log(proxy.name); // Felix
+// console.log(proxy.age); // 24
+// console.log(proxy.class); // property does not exist
+// }
 }
